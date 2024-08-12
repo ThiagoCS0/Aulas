d6d903b1bc7a8e7144534aae348d3558cb558b77
Thiago C Silva <tkreserva@gmail.com>
2024-08-12 18:51:37 -0300

    Aula 33 React, melhorando o JS

--- Aula33/script.js
+++ Aula33/script.js
@@ -15,23 +15,32 @@ const conteudos = [
 		"Austrália, me lembra de canguro."
 	]
 ]
+
 const
 	btnTab1 = document.getElementById('btn-tab1'),
-	btnTab2 = document.getElementById('btn-tab1'),
-	btnTab3 = document.getElementById('btn-tab1'),
+	btnTab2 = document.getElementById('btn-tab2'),
+	btnTab3 = document.getElementById('btn-tab3'),
 	content = document.getElementById('content');
-function displayContent(itens) {
 
+function displayContent(itens) {
+	let listaConteudo = ""
+	for (const item of itens) { listaConteudo += `<li>${item}</li>`; }
+	const lista = document.createElement('ul');
+	content.innerHTML = listaConteudo;
+	content.append(lista);
 }
+
 function activateButton(btn) {
 	btnTab1.className = btnTab2.className = btnTab3.className = "";
 	btn.className = 'active'
 }
+
 function handleClick(event) {
 	const btnId = event.target.id;
 	activateButton(event.target);
-	if (btnId == 'btn-tab1') { displayContent(content[0]); }
+	displayContent(conteudos[btnId == 'btn-tab1' ? 0 : btnId == 'btn-tab2' ? 1 : 2]);
 }
+displayContent(conteudos[0]);
 btnTab1.addEventListener('click', handleClick);
 btnTab2.addEventListener('click', handleClick);
 btnTab3.addEventListener('click', handleClick);
