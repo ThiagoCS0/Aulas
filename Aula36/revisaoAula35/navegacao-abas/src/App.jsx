af75a879eb753500209c65594d90692ba3158535
Thiago C Silva <tkreserva@gmail.com>
2024-08-18 13:10:32 -0300

    Aula 36 - TimerComponent

--- Aula36/revisaoAula35/navegacao-abas/src/App.jsx
+++ Aula36/revisaoAula35/navegacao-abas/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import MyComponent from '../../../classComponent/ClassComponent'
+import TimerComponent from '../../../classComponent/TimerComponent'
 // import NavegacaoAbas from './components/navegacao-aba'
 
 function App() {
   return (
     <>
-      <MyComponent />
+      {/* <MyComponent /> */}
+      <TimerComponent />
     </>
   )
 }
