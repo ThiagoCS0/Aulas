63873288fe1c1b8bdcf68f325ad086089f33e088
Thiago C Silva <tkreserva@gmail.com>
2024-08-18 09:31:53 -0300

    Counter, Mirror, ToDoList

--- Aula35/stateHooks/src/main.jsx
+++ Aula35/stateHooks/src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
+  // Interessante apara debugar: diz quando está usando código despreciado
+  //O modo estrito (Strict Mode) é uma ferramenta para sinalizar potenciais problemas em uma aplicação. 
   <StrictMode>
     <App />
   </StrictMode>,
