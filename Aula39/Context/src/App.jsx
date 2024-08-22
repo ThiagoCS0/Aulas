12b3140aef1b1d577a6fd76881d15bcc3039dbfe
Thiago C Silva <tkreserva@gmail.com>
2024-08-22 17:41:23 -0300

    Aula 39 - Theme

--- Aula39/Context/src/App.jsx
+++ Aula39/Context/src/App.jsx
@@ -1,13 +1,32 @@
-import MyChildren from "./components/Filho"
-import { MyProvider } from "./components/Contexto"
+// import MyChildren from "./components/Filho"; import { MyProvider } from "./components/Contexto";]
+import { useState, createContext, useContext } from "react"
+const ThemeContext = createContext({ theme: 'light', toggleTheme: () => { } })
+
 function App() {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    setTheme(currentTheme => currentTheme == 'light' ? 'dark' : 'light')
+  }
 
   return (
     <>
-      <MyProvider>
-        <MyChildren />
-      </MyProvider>
+      {/* <MyProvider> <MyChildren /> </MyProvider> */}
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <div>
+          <Toolbar />
+          <button onClick={toggleTheme}>Troca tema</button>
+        </div>
+      </ThemeContext.Provider>
     </>
+  )
+}
+
+function Toolbar() {
+  const { theme } = useContext(ThemeContext)
+  return (
+    <div style={{ background: theme == 'dark' ? '#000' : '#AAA', color: theme == 'dark' ? '#FFF' : '#000', padding: '20px', borderRadius: '4px', margin: '8px' }}>
+      Tema utilizado - {theme}
+    </div>
   )
 }
 
